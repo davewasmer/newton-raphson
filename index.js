1ad8fc29c3534ba9c16ecae82bc672ae7ef0524c
Dave Wasmer <davewasmer@gmail.com>
2014-12-23 21:12:38 -0700

    added UMD wrapper, version bump

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-module.exports = findRoot;
-
-function findRoot(f, fprime, guess, options) {
-  options = options || {};
-  var tolerance = options.tolerance || 0.00000001;
-  var epsilon = options.epsilon || 0.0000000000001;
-  var maxIterations = options.maxIterations || 20;
-  var haveWeFoundSolution = false; 
-  var newtonX;
- 
-  for (var i = 0; i < maxIterations; ++i) {
-    var denominator = fprime(guess);
-    if (Math.abs(denominator) < epsilon) {
-      return false
-    }
- 
-    result = guess - (f(guess) / denominator);
-    
-    var resultWithinTolerance = Math.abs(result - guess) < tolerance;
-    if (resultWithinTolerance) { 
-      return result
-    }
-
-    guess = result;
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.returnExports = factory();
   }
-  
-  return false;
-}
+}(this, function () {
+  return function findRoot(f, fprime, guess, options) {
+    options = options || {};
+    var tolerance = options.tolerance || 0.00000001;
+    var epsilon = options.epsilon || 0.0000000000001;
+    var maxIterations = options.maxIterations || 20;
+    var haveWeFoundSolution = false; 
+    var newtonX;
+   
+    for (var i = 0; i < maxIterations; ++i) {
+      var denominator = fprime(guess);
+      if (Math.abs(denominator) < epsilon) {
+        return false
+      }
+   
+      result = guess - (f(guess) / denominator);
+      
+      var resultWithinTolerance = Math.abs(result - guess) < tolerance;
+      if (resultWithinTolerance) { 
+        return result
+      }
+
+      guess = result;
+    }
+    
+    return false;
+  }
+}));
