81f8fd94269ac2db4046a18434f932ad21c22572
Dave Wasmer <davewasmer@gmail.com>
2014-12-23 20:57:58 -0700

    added implementation

---
+++ index.js
@@ -0,0 +1,28 @@
+module.exports = findRoot;
+
+function findRoot(f, fprime, guess, options) {
+  options = options || {};
+  var tolerance = options.tolerance || 0.00000001;
+  var epsilon = options.epsilon || 0.0000000000001;
+  var maxIterations = options.maxIterations || 20;
+  var haveWeFoundSolution = false; 
+  var newtonX;
+ 
+  for (var i = 0; i < maxIterations; ++i) {
+    var denominator = fprime(guess);
+    if (Math.abs(denominator) < epsilon) {
+      return false
+    }
+ 
+    result = guess - (f(guess) / denominator);
+    
+    var resultWithinTolerance = Math.abs(result - guess) < tolerance;
+    if (resultWithinTolerance) { 
+      return result
+    }
+
+    guess = result;
+  }
+  
+  return false;
+}
